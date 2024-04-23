4a83e3508d10e1750a391f550c2b66e51572d273
Jeremy <jeremyguo2000@gmail.com>
2024-04-23 09:19:31 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Xiuzhen Guo on 4/23/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
