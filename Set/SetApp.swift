2f78d29d65292ab05b17e8056a9b448325cb4079
Jeremy <jeremyguo2000@gmail.com>
2024-05-06 16:40:09 +0800

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Xiuzhen Guo on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
